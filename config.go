2ccbf1d3a27cd07b9c67c62c88775f9a7cda8979
pujie@kingsoft.com <pujie19861214>
2018-05-30 14:42:25 +0800

    aliyun log hub

--- config.go
+++ config.go
@@ -91,6 +91,6 @@ func getLevel0(flag string, defaultFlag int) int {
 			return i
 		}
 	}
-	fmt.Printf("[ERROR] 未找到合适的日志级别，使用默认值:%d \n", defaultFlag)
+	fmt.Printf("[ERROR] 未找到合适的日志级别[%s]，使用默认值:%d \n", flag, defaultFlag)
 	return defaultFlag
 }
